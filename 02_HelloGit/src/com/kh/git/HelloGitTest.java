185a7040d0ba7525ac28d00d16da1b9c2746fe5a
dbwjd0618 <59955291+dbwjd0618@users.noreply.github.com>
2020-01-16 21:02:53 +0900

    to 정우에게

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -7,5 +7,7 @@ public static void main(String[] args) {
 	System.out.println("허파고 바보");
 	System.out.println("조정우는 천재");
 	System.out.println("조개일은 고스톱쟁이");
+	System.out.println("정우여친없어여");
+	
 }
 }
