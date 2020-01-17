681afa32a2c9bda7b9e0cda67ea40a16db7bd2b1
JeongWooCHO <58026300+JeongWooCHO@users.noreply.github.com>
2020-01-17 12:42:59 +0900

    hoho
    i got U

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -13,6 +13,11 @@ public static void main(String[] args) {
 	System.out.println("정우여친있지 않아아아아아?");
 	System.out.println("지인짜아아아 없어????");
 	System.out.println("승환형도 여친없는 걸 ???낄렵낄렵.");
+	System.out.println("김유정 바보");
+	System.out.println("허파고 바보");
+	System.out.println("조정우는 천재");
+	System.out.println("조개일은 고스톱쟁이");
+	System.out.println("조개일은 고스톱쟁이123");
 	
 }
 }
